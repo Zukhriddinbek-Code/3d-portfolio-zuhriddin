36ef6ee460bdbdc4e229cd46de03c360f952b09f
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-07-27 22:22:24 +0900

    starting our about component by importing some files

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,13 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable no-unused-vars */
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
 const About = () => {
   return <div>About</div>;
 };
