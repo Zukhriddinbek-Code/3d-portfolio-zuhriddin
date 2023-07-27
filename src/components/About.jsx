cbef0343ccc7438fbb6c0983916fc2ba6c80ab34
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-07-27 22:15:47 +0900

    about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable no-unused-vars */
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  return <div>About</div>;
+};
 
-export default About
+export default About;
