ba1c8ff8871916b831cd170da11d569e70321204
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-07-30 16:04:14 +0900

    ball canvas developing

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable no-unused-vars */
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((tech) => (
+        <div key={tech.name} className="w-28 h-28">
+          <BallCanvas icon={tech.icon} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Tech
+export default SectionWrapper(Tech, "");
