30a5600786fcbea5084dcb7d64f394f480f7b2ef
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-07-30 18:13:38 +0900

    initialising the works componnet by importing some libs

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable no-unused-vars */
+
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Works = () => {
-  return (
-    <div>Works</div>
-  )
-}
+  return <div>Works</div>;
+};
 
-export default Works
+export default Works;
